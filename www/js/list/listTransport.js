7031e54da0368ae794f01f53b8f216e5b466ed4d
Jonathan <jonathanlow.2013@sis.smu.edu.sg>
2015-10-01 10:34:25 +0800

    upload partial codes for listing transport activities on listing page

--- www/js/list/listTransport.js
+++ www/js/list/listTransport.js
@@ -1,12 +1,26 @@
 angular.module('crowdsourcing')
 
     .controller('listTransportController', function ($scope, $ionicPopup, $state, $http, $jrCrop) {
+      $scope.transportID=[];
+      $scope.transportName=[];
+      $scope.transportDateTimeStart=[];
+
       $http.get("http://localhost/RetrieveTransportActivity.php")
       .success(function (data) {
         var transportDetails = data;
 
         if (transportDetails != null) {
-          console.log(transportDetails);
+          for(var i = 0; i<transportDetails.length; i++)
+          {
+            if(transportDetails[i].TransportID != null && transportDetails[i].ActivityName && transportDetails[i].DateTimeStart)
+            {
+              $scope.transportID.push(transportDetails[i].TransportID);
+              $scope.transportName.push(transportDetails[i].ActivityName);
+              $scope.transportDateTimeStart.push(transportDetails[i].DateTimeStart);
+            }
+          }
+
+          console.log($scope.transportName);
 
           /*
           for (var i = 0; i < loginDetails.length; i++) {

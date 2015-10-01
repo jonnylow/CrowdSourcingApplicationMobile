07c9fac9a77680e24df9417e0b90863713b97c54
Jonathan <jonathanlow.2013@sis.smu.edu.sg>
2015-10-01 19:17:31 +0800

    Note Leejia & Clara: i commented off background img for time being at style.css to display the google map.
    Done listing of transport activities (for nearby map) - outstanding:
    1) remove activities that pick up location that are too far away
    
    Linked up to transport activities with google map (for nearby map - scan page) - outstanding:
    1) customize marker
    2) link up map marker with listing box on page
    3) put transport activities at pickup location into a single marker

--- www/js/list/listTransport.js
+++ www/js/list/listTransport.js
@@ -1,11 +1,20 @@
 angular.module('crowdsourcing')
 
     .controller('listTransportController', function ($scope, $ionicPopup, $state, $http, $jrCrop) {
+      $scope.myLocation = {lng : '', lat: ''};
       $scope.transportID=[];
       $scope.transportName=[];
       $scope.transportDateTimeStart=[];
+      $scope.transportFromDistance=[];
 
-      $http.get("http://localhost/RetrieveTransportActivity.php")
+    $scope.getLocation= function (position) {
+      $scope.$apply(function () {
+        $scope.myLocation.lng = position.coords.longitude;
+        $scope.myLocation.lat = position.coords.latitude;
+      });
+    }
+
+    $http.get("http://localhost/RetrieveTransportActivity.php")
       .success(function (data) {
         var transportDetails = data;
 
@@ -14,29 +23,21 @@ angular.module('crowdsourcing')
           {
             if(transportDetails[i].TransportID != null && transportDetails[i].ActivityName && transportDetails[i].DateTimeStart)
             {
+              //calculate distance & format date/time
+              $scope.temp =transportDetails[i].DateTimeStart.split(' ');
+              var from = new google.maps.LatLng($scope.myLocation.lat, $scope.myLocation.lng);
+              var to = new google.maps.LatLng(parseFloat(transportDetails[i].LocationFromLat), parseFloat(transportDetails[i].LocationFromLong));
+              var m = google.maps.geometry.spherical.computeDistanceBetween(from, to).toFixed(2);
+              var km = (m/1000).toFixed(2);
+
               $scope.transportID.push(transportDetails[i].TransportID);
               $scope.transportName.push(transportDetails[i].ActivityName);
-              $scope.transportDateTimeStart.push(transportDetails[i].DateTimeStart);
+              $scope.transportDateTimeStart.push("Date/Time: " + $scope.temp[0] + " | " + $scope.temp[1]);
+              $scope.transportFromDistance.push(m + " m" + " OR "+ km);
             }
           }
-
-          console.log($scope.transportName);
-
-          /*
-          for (var i = 0; i < loginDetails.length; i++) {
-            if(tempNRIC == loginDetails[i].Email && tempPassword == loginDetails[i].Password)
-            {
-              loginCheck = 1;
-
-              window.localStorage.setItem("loginUserName", loginDetails[i].Name);
-              window.localStorage.setItem("loginUserEmail", loginDetails[i].Email);
-              window.localStorage.setItem("loginUserPassword", loginDetails[i].Password);
-              window.localStorage.setItem("loginUserContactNumber", loginDetails[i].Phone);
-              window.localStorage.setItem("loginUserDOB", loginDetails[i].DOB);
-
-              $state.go('tab.home', {}, {reload: true});
-            }
-          }*/
         }
       })
+
+      navigator.geolocation.getCurrentPosition($scope.getLocation);
     });
--- www/js/scan/scan.js
+++ www/js/scan/scan.js
@@ -1,42 +1,79 @@
 angular.module('crowdsourcing')
 
     .controller('scanController', function ($scope, $ionicPopup, $state, $http, $jrCrop) {
-        $scope.myLocation = {
-          lng : '',
-          lat: ''
-        }
-        $scope.drawMap = function(position) {
-          //$scope.$apply is needed to trigger the digest cycle when the geolocation arrives and to update all the watchers
-          $scope.$apply(function() {
-            $scope.myLocation.lng = position.coords.longitude;
-            $scope.myLocation.lat = position.coords.latitude;
+        $scope.myLocation = {lng : '', lat: ''};
+        $scope.transportID=[];
+        $scope.transportName=[];
+        $scope.transportDateTimeStart=[];
+        $scope.transportFromDistance=[];
+        $scope.markers = [];
 
-            $scope.map = {
-              center: {
-                latitude: $scope.myLocation.lat,
-                longitude: $scope.myLocation.lng
-              },
-              zoom: 11,
-              pan: 1
-            };
+      //plot map
+        if($scope.transportID != null && $scope.transportName != null && $scope.transportDateTimeStart !=null) {
+          $scope.drawMap = function (position) {
+            //$scope.$apply is needed to trigger the digest cycle when the geolocation arrives and to update all the watchers
+            $scope.$apply(function () {
+              $scope.myLocation.lng = position.coords.longitude;
+              $scope.myLocation.lat = position.coords.latitude;
 
-            $scope.marker = {
-              id: 0,
-              coords: {
-                latitude: $scope.myLocation.lat,
-                longitude: $scope.myLocation.lng
+              $scope.map = {
+                center: {
+                  latitude: $scope.myLocation.lat,
+                  longitude: $scope.myLocation.lng
+                },
+                zoom: 11,
+                pan: 1
+              };
+
+              var currentLocationMarker = {
+                id: 0,
+                coords: {
+                  latitude: $scope.myLocation.lat,
+                  longitude: $scope.myLocation.lng
+                }
+              };
+
+              $scope.markers.push(currentLocationMarker);
+            });
+          }
+
+          //retrieve from DB
+          $http.get("http://localhost/RetrieveTransportActivity.php")
+            .success(function (data) {
+              var transportDetails = data;
+
+              if (transportDetails != null) {
+                for(var i = 0; i<transportDetails.length; i++)
+                {
+                  if(transportDetails[i].TransportID != null && transportDetails[i].ActivityName && transportDetails[i].DateTimeStart
+                    && transportDetails[i].LocationFromLat !=null && transportDetails[i].LocationFromLong != null)
+                  {
+                    //calculate distance & format date/time
+                    $scope.temp =transportDetails[i].DateTimeStart.split(' ');
+                    var from = new google.maps.LatLng($scope.myLocation.lat, $scope.myLocation.lng);
+                    var to = new google.maps.LatLng(parseFloat(transportDetails[i].LocationFromLat), parseFloat(transportDetails[i].LocationFromLong));
+                    var m = google.maps.geometry.spherical.computeDistanceBetween(from, to).toFixed(2);
+                    var km = (m/1000).toFixed(2);
+
+                    $scope.transportID.push(transportDetails[i].TransportID);
+                    $scope.transportName.push(transportDetails[i].ActivityName);
+                    $scope.transportDateTimeStart.push("Date/Time: " + $scope.temp[0] + " | " + $scope.temp[1]);
+                    $scope.transportFromDistance.push(m + " m" + " OR "+ km);
+
+
+                    var tempMarker = {
+                      id: i+1,
+                      coords: {
+                        latitude: parseFloat(transportDetails[i].LocationFromLat),
+                        longitude: parseFloat(transportDetails[i].LocationFromLong)
+                      }
+                    };
+                    $scope.markers.push(tempMarker);
+                  }
+                }
               }
-            };
+            })
 
-            $scope.marker1 = {
-              id: 1,
-              coords: {
-                latitude: 45,
-                longitude: -73
-              }
-            };
-          });
+          navigator.geolocation.getCurrentPosition($scope.drawMap);
         }
-
-        navigator.geolocation.getCurrentPosition($scope.drawMap);
     });

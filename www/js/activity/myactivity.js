7744ed43d0790d10f8d2f918c7de9445b6405ab9
lingyi <lychiang.2013@sis.smu.edu.sg>
2015-10-03 23:59:24 +0800

    MyActivity - Is now working Lacking 1) dynamic status 2) history page

--- www/js/activity/myactivity.js
+++ www/js/activity/myactivity.js
@@ -1,8 +1,10 @@
 angular.module('crowdsourcing')
 
     .controller('myactivityController', function ($scope, $ionicPopup, $state, $http, $jrCrop, $stateParams, $ionicHistory) {
-    	$scope.transportName=[];
+        $scope.transportID=[];
+        $scope.transportName=[];
       	$scope.transportDateTimeStart=[];
+
     	if(window.localStorage.getItem("loginUserContactNumber") != null) {
         $scope.phone = window.localStorage.getItem("loginUserContactNumber");
         }
@@ -10,18 +12,18 @@ angular.module('crowdsourcing')
           $state.go('login', {}, {reload: true});
         }
 
-       	var urlString = "http://localhost/AddUserAccount.php?phone="+$scope.phone;
-        
+       	var urlString = "http://localhost/RetrieveTransportByUser.php?phone="+$scope.phone;
+
        	$http.get(urlString)
       	.success(function (data) {
         var transportDetails = data;
-
-        if (!transportDetails){
+        if (transportDetails != null){
         	for(var i = 0; i<transportDetails.length; i++){
         		if(transportDetails[i].TransportID != null && transportDetails[i].ActivityName && transportDetails[i].DateTimeStart){
+              var temp =transportDetails[i].DateTimeStart.split(' ');
         			$scope.transportID.push(transportDetails[i].TransportID);
         			$scope.transportName.push(transportDetails[i].ActivityName);
-              		$scope.transportDateTimeStart.push("Date/Time: " + $scope.temp[0] + " | " + $scope.temp[1]);
+              $scope.transportDateTimeStart.push("Date/Time: " + temp[0] + " | " + temp[1]);
         		}
         	}
         }

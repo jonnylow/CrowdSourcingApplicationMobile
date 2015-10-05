a461b73c3bfc54bb15ba4760d997c4adbf85839e
Jonathan <jonathanlow.2013@sis.smu.edu.sg>
2015-10-05 20:25:42 +0800

    add in new styles and change login to suit new requirements.

--- webservices database/AddUserAccount.php
+++ webservices database/AddUserAccount.php
@@ -2,7 +2,7 @@
 $servername = "www.changhuapeng.com";
 $username = "volunteer";
 $password = "iamaguest";
-$dbname = "volunteer";
+$dbname = "laravel";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,29 +11,34 @@ $dbname = "volunteer";
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-
+		
 	 // prepare and bind
-	$stmt = $conn->prepare("INSERT INTO VolunteerCFS (`Phone`, `Name`, `Email`, `Password`, `DOB`, `HaveCar`, `KnowCPR`) VALUES (?,?,?,?,?,?,?)");
-	$stmt->bind_param("issssii", $phone, $name, $email, $password, $dob, $haveCar, $knowCPR);
+	$stmt = $conn->prepare("INSERT INTO volunteers (`nric`,`name`,`email`,`password`,`gender`,`date_of_birth`,`contact_no`,`occupation`,`has_car`,`area_of_preference_1`,`area_of_preference_2`,`image_nric_front`,`image_nric_back`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+	$stmt->bind_param("sssssssssssss", $nric, $name, $email, $password, $gender, $dateOfBirth, $contactNo, $occupation, $hasCar, $areaPreferences1, $areaPreferences2, $nricFront, $nricBack);
 	
 	//get parameters
-	$phone = $_GET['phone'];
+	$nric = $_GET['nric'];
 	$name = $_GET['name'];
 	$email = $_GET['email'];
 	$password = $_GET['password'];
-	$dob = $_GET['dob'];
-	$haveCar= $_GET['haveCar'];
-	$knowCPR = $_GET['knowCPR'];
-
+	$gender = $_GET['gender'];
+	$dateOfBirth= $_GET['dob'];
+	$contactNo = $_GET['phone'];
+	$occupation= $_GET['occupation'];
+	$hasCar= $_GET['haveCar'];
+	$areaPreferences1= $_GET['preferences1'];
+	$areaPreferences2= $_GET['preferences2'];
+	$nricFront= $_GET['frontIC'];
+	$nricBack= $_GET['backIC'];
 	
 	$passed = $stmt->execute();
-
+	
 	if($passed){
 		$a = array("status" => array("Created successfully")); 
 		$json_string = json_encode($a, JSON_PRETTY_PRINT);
 		echo $json_string;
 	} else {
-		$a = array("status" => array("Contact number exists")); 
+		$a = array("status" => array("error")); 
 		$json_string = json_encode($a, JSON_PRETTY_PRINT);
 		echo $json_string;
 	}
--- www/js/account/moreQuestions.js
+++ www/js/account/moreQuestions.js
@@ -6,51 +6,62 @@ angular.module('crowdsourcing')
       $scope.tempPassword = window.localStorage.getItem("tempPassword");
       $scope.tempContactNumber = window.localStorage.getItem("tempContactnumber");
       $scope.tempDOB = window.localStorage.getItem("tempDOB");
+    $scope.tempNRIC = window.localStorage.getItem("tempNRIC");
+    $scope.tempGender = window.localStorage.getItem("tempGender");
+    $scope.tempFrontIC = window.localStorage.getItem("tempFrontIC");
+    $scope.tempBackIC = window.localStorage.getItem("tempBackIC");
 
-      $scope.fields= {carChecked: false, cprChecked:false};
+      $scope.fields= {carChecked: false, occupation:""};
 
       $scope.verify = function(fields)
       {
         if(fields != null) {
-          if(fields.carChecked == true) {
-            $scope.tempCarChecked = 1;
+          if (fields.occupation!= null && fields.occupation.trim() != "")
+          {
+            if(fields.carChecked == true) {
+              $scope.tempCarChecked = 1;
+            }
+            else{
+              $scope.tempCarChecked = 0;
+            }
+
+            window.localStorage.removeItem("tempName");
+            window.localStorage.removeItem("tempEmail");
+            window.localStorage.removeItem("tempPassword");
+            window.localStorage.removeItem("tempContactnumber");
+            window.localStorage.removeItem("tempDOB");
+            window.localStorage.removeItem("tempNRIC");
+            window.localStorage.removeItem("tempGender");
+            window.localStorage.removeItem("tempFrontIC");
+            window.localStorage.removeItem("tempBackIC");
+
+            var urlString = "http://localhost/AddUserAccount.php?phone="+$scope.tempContactNumber+"&name="+$scope.tempName+"&email="+$scope.tempEmail+"&password="+$scope.tempPassword+"&dob="+$scope.tempDOB
+              +"&nric="+$scope.tempNRIC+"&gender="+$scope.tempGender+"&frontIC="+$scope.tempFrontIC + "&backIC="+$scope.tempBackIC+"&haveCar="+$scope.tempCarChecked+"&preferences1="+fields.preferences_1
+              +"&preferences2="+fields.preferences_2+"&occupation="+fields.occupation;
+
+
+            $http.get(urlString)
+              .success(function (data) {
+
+                var status = data;
+                if (status != null) {
+                  var alertPopup = $ionicPopup.alert({
+                    title: 'Status',
+                    template: status.status[0]
+                  });
+
+                  $state.go('login', {}, {reload: true});
+                }
+              })
+
+              .error(function (data) {
+                alert("Error in connection");
+              });
           }
-          else{
-            $scope.tempCarChecked = 0;
+          else
+          {
+            alert("Please fill in all fields.");
           }
-
-          if(fields.cprChecked == true) {
-            $scope.tempCPRChecked = 1;
-          }
-          else{
-            $scope.tempCPRChecked = 0;
-          }
-
-          window.localStorage.removeItem("tempName");
-          window.localStorage.removeItem("tempEmail");
-          window.localStorage.removeItem("tempPassword");
-          window.localStorage.removeItem("tempContactnumber");
-          window.localStorage.removeItem("tempDOB");
-
-          var urlString = "http://localhost/AddUserAccount.php?phone="+$scope.tempContactNumber+"&name="+$scope.tempName+"&email="+$scope.tempEmail+"&password="+$scope.tempPassword+"&dob="+$scope.tempDOB+"&haveCar="+$scope.tempCarChecked+"&knowCPR="+$scope.tempCPRChecked;
-
-          $http.get(urlString)
-            .success(function (data) {
-
-               var status = data;
-               if (status != null) {
-               var alertPopup = $ionicPopup.alert({
-               title: 'Status',
-               template: status.status[0]
-               });
-
-                 $state.go('login', {}, {reload: true});
-               }
-            })
-
-            .error(function (data) {
-               alert("Error in connection");
-               });
         }
         else
         {

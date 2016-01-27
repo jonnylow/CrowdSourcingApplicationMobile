ccfad6133a640824a6b0619a529c207dc38d5753
Jonathan <jonathanlow.2013@sis.smu.edu.sg>
2016-01-27 15:04:56 +0800

    change faq answers

--- www/js/help/help.js
+++ www/js/help/help.js
@@ -5,9 +5,9 @@ angular.module('crowdsourcing')
       $scope.groups = [];
 
       $scope.groups.push({name: "Do i have to stay for the whole duration?", answer: "Yes you have to. All transportation activities require volunteers to be with the senior all the way until they are safely brought back to the home and is accounted for."});
-      $scope.groups.push({name: "How do i withdraw from an activity?", answer: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."});
-      $scope.groups.push({name: "Why is this just for Henderson Home?", answer: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."});
-      $scope.groups.push({name: "Who do i contact in case of any emergency?", answer: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."});
+      $scope.groups.push({name: "How do i withdraw from an activity?", answer: "Coming Soon ..."});
+      $scope.groups.push({name: "Why is this just for Henderson Home?", answer: "Coming Soon ..."});
+      $scope.groups.push({name: "Who do i contact in case of any emergency?", answer: "Coming Soon ..."});
 
 
     $scope.toggleGroup = function(group) {

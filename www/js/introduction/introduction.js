339ad9b78e9f22320580962c94b9ca52e49a40df
lingyi <lychiang.2013@sis.smu.edu.sg>
2016-03-03 13:55:37 +0800

    Edited Index.html , App.js / Menu_popout.html Added Introduction.js/ introduction,html , aboutus.html Updated index and app for re-redicting within pages

---
+++ www/js/introduction/introduction.js
@@ -0,0 +1,15 @@
+angular.module('crowdsourcing')
+
+    .controller('introductionController', function ($scope, $ionicPopup, $state, $http, $jrCrop, $ionicHistory, $ionicLoading, $ionicHistory,apiUrl,$ionicSlideBoxDelegate) {
+        $scope.$on('slideBox.slideHasChanged', function() {
+            slider.slide(index);
+      });
+       $scope.$on('slideBox.nextSlide', function() {
+        slider.next();
+      });
+
+      $scope.$on('slideBox.prevSlide', function() {
+        slider.prev();
+      });
+
+    });

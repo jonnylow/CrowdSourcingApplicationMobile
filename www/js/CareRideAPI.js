198c26b213cf1f9a3dd2baa554e8a36b8730f57d
Jonathan <jonathanlow.2013@sis.smu.edu.sg>
2016-03-31 00:24:00 +0800

    change back to staging server for internal testing.

--- www/js/CareRideAPI.js
+++ www/js/CareRideAPI.js
@@ -1,5 +1,5 @@
 angular.module('CareRideAPI.config', [])
 //.constant('apiUrl', "http://52.76.2.82/careguide/api/"); old ida server
-//.constant('apiUrl', "http://changhuapeng.com/laravel/api/");
-.constant('apiUrl', "http://52.77.41.63/careguide/api/"); //new ida server
+.constant('apiUrl', "http://changhuapeng.com/laravel/api/");
+//.constant('apiUrl', "http://52.77.41.63/careguide/api/"); //new ida server
 

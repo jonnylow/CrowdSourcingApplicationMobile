52beedb960cbec9ae27a351df1a53abab011cad8
Jonathan <jonathanlow.2013@sis.smu.edu.sg>
2016-04-09 16:01:00 +0800

    change deployment server

--- www/js/CareRideAPI.js
+++ www/js/CareRideAPI.js
@@ -1,4 +1,4 @@
 angular.module('CareRideAPI.config', [])
-.constant('apiUrl', "http://changhuapeng.com/laravel/api/");
-//.constant('apiUrl', "http://52.77.41.63/careguide/api/"); //new ida server
+//.constant('apiUrl', "http://changhuapeng.com/laravel/api/");
+.constant('apiUrl', "http://52.77.41.63/careguide/api/"); //new ida server
 
